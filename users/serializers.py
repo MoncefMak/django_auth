f343dcacebb03c759f750886126da45e12174862
MoncefMak <72460183+MoncefMak@users.noreply.github.com>
2023-10-02 12:54:18 +0100

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,56 +1,35 @@
-itfrom rest_framework import serializers
-
+from rest_framework import serializers
 from users.models import User
 
-
-class UserSerializer(serializers.ModelSerializer):
+class BaseUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "email", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create(
-            email=validated_data['email'],
-            username=validated_data['username'],
-            role=validated_data['role']
-        )
+        role = self.Meta.role
+        is_staff = self.Meta.is_staff
+        validated_data['role'] = role
+        validated_data['is_staff'] = is_staff
+        user = User.objects.create(**validated_data)
         user.set_password(validated_data['password'])
         user.save()
         return user
 
-
-class CustomerSerializer(UserSerializer):
+class CustomerSerializer(BaseUserSerializer):
     class Meta:
-        model = User
-        fields = ["id", "email", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
+        role = User.Role.CUSTOMER
+        is_staff = False
 
-    def create(self, validated_data):
-        validated_data['role'] = User.Role.CUSTOMER
-        validated_data['is_staff'] = False
-        return super().create(validated_data)
-
-
-class StuffSerializer(UserSerializer):
+class StuffSerializer(BaseUserSerializer):
     class Meta:
-        model = User
-        fields = ["id", "email", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
+        role = User.Role.STUFF
+        is_staff = False
 
-    def create(self, validated_data):
-        validated_data['role'] = User.Role.STUFF
-        validated_data['is_staff'] = False
-        return super().create(validated_data)
-
-
-class SuperAdminSerializer(UserSerializer):
+class SuperAdminSerializer(BaseUserSerializer):
     class Meta:
-        model = User
         fields = ["id", "email", "username", "password", "role"]
         extra_kwargs = {"password": {"write_only": True}}
-
-    def create(self, validated_data):
-        validated_data['role'] = User.Role.SUPERADMIN
-        validated_data['is_staff'] = True
-        return super().create(validated_data)
+        role = User.Role.SUPERADMIN
+        is_staff = True
